b66034f0c4e91c43ee055e3791d3509269c093ba
Excelius <excelius@excelius@qq.com>
2025-09-24 23:30:15 +0800

    重构删除链表倒数第n个节点的代码，优化注释并增加示例说明

--- problems/linked_list/No_19_删除链表的倒数第n个结点.py
+++ problems/linked_list/No_19_删除链表的倒数第n个结点.py
@@ -1,23 +1,46 @@
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        node_list = list()
+        """
+        这个题解的思路是，把所有的节点都放到一个列表里，然后这样就可以按照下标进行操作了
+        """
+        dummy = ListNode(0, head)
+        node_list = [dummy]  # 下标 0 就是哑结点
         cur = head
-        num = 0
         while cur:
             node_list.append(cur)
-            num += 1
             cur = cur.next
+        node_list.append(None)  # 备用
 
-        if num == 1:
-            return
+        m = len(node_list) - 2  # 实际节点个数
+        # 不管删的是谁，前驱都在 node_list[m - n]
+        node_list[m - n].next = node_list[m - n + 2]  # 跳过被删节点
+        return dummy.next
 
-        # 删除倒数第 n 个
-        pre = node_list[num - n - 1]
-        need_deleted = node_list[num - n]
-        nxt = node_list[num - n + 1]
-        pre.next = nxt
+
+class Solution2:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        """
+        最佳题解：使用快慢指针，快指针先走 n + 1, 然后一起走，快指针走到 None, 慢指针就走到了要删除节点的前一个位置
+        """
+        dummy = ListNode(0, head)
+        fast, slow = dummy, dummy
+
+        for i in range(n + 1):
+            fast = fast.next
+
+        while fast:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+
         return head
